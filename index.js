2a3179b92450e02cd3ec00b1c3d515991819b625
Patel dhyeykumar Rajubhai <dhyeybhuva2003@gmail.com>
2024-06-04 09:32:17 +0530

    Update index.js
    Porte changes

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use('/contacts', contactRoutes);
 app.use('/circulars', circularRoutes);
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 9001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
