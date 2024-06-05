5b3642dc329c076722879428e02a8ef187efbcf7
Patel dhyeykumar Rajubhai <dhyeybhuva2003@gmail.com>
2024-06-05 14:55:30 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -15,8 +15,8 @@ const connectDB = async () => {
 
         // Connect to MongoDB
         const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true, // This can be removed if it causes warnings
-            useCreateIndex: true, // If you're using older mongoose versions, this can be removed in newer versions
+            // useNewUrlParser: true, // This can be removed if it causes warnings
+            // useCreateIndex: true, // If you're using older mongoose versions, this can be removed in newer versions
             useFindAndModify: false
         });
 
