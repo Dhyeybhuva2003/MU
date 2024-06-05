38f711f5c1fa481a1dd7d948e9932aa39319e0af
Patel dhyeykumar Rajubhai <dhyeybhuva2003@gmail.com>
2024-06-05 15:00:39 +0530

    last update

--- config/db.js
+++ config/db.js
@@ -16,7 +16,7 @@ const connectDB = async () => {
         // Connect to MongoDB
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
-            useCreateIndex: true
+            useUnifiedTopology: true
         });
 
         console.log(`MongoDB Connected: ${conn.connection.host}`);
