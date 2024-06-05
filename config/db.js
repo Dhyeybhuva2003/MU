62f5c95268cc9da56ffe61047af8d118ef463a26
Patel dhyeykumar Rajubhai <dhyeybhuva2003@gmail.com>
2024-06-05 14:47:45 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -15,7 +15,7 @@ const connectDB = async () => {
 
         // Connect to MongoDB
         const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
+            // useNewUrlParser: true,
             useUnifiedTopology: true
         });
 
