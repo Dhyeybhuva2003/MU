3146eff8a8bd611bd8218b29244e6186c0f3a01f
Dhyeybhuva2003 <dhyeybhuva2003@gmail.com>
2024-06-06 14:21:22 +0530

    add midelwer

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-// project-root/app.js
-
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors'); // Add this line
+
 const placementRecordRoutes = require('./routes/placementRecordRoutes');
 const homeUpdateRoutes = require('./routes/homeUpdateRoutes');
 const academicCalendarRoutes = require('./routes/academicCalendarRoutes');
 const examScheduleRoutes = require('./routes/examScheduleRoutes');
 const contactRoutes = require('./routes/contactRoutes');
 const circularRoutes = require('./routes/circularRoutes');
-const authRoutes = require('./routes/authRoutes'); 
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
+// Middleware
+app.use(cors({
+    origin: 'https://mu-l50w.onrender.com',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization'
+}));
+
+app.use(express.json());
+
 // Connect to MongoDB using db.js
 require('./config/db')();
-
-// Middleware
-app.use(express.json());
 
 // Routes
 app.use('/home-updates', homeUpdateRoutes);
@@ -25,7 +31,7 @@ app.use('/academic-calendars', academicCalendarRoutes);
 app.use('/exam-schedules', examScheduleRoutes);
 app.use('/contacts', contactRoutes);
 app.use('/circulars', circularRoutes);
-app.use('/auth', authRoutes);  // Add this line
+app.use('/auth', authRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 9001;
