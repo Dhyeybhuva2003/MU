305d8ad5f6ea0cd5fd5958b2bf39191bbaf6fed1
Patel dhyeykumar Rajubhai <dhyeybhuva2003@gmail.com>
2024-06-08 21:27:33 +0530

    chage the password

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,7 +15,7 @@ const register = async (req, res) => {
         // const hashedPassword = await bcrypt.hash(password, 10);
 
         // Create a new user
-        const user = new User({ username, email, password: hashedPassword });
+        const user = new User({ username, email, password });
         await user.save();
 
         res.status(201).json({ message: 'User registered successfully' });
